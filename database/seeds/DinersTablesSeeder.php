466281edfc10257dbf86d5c6ef470a064171f73e
fitodark <fitodark68@gmail.com>
2024-01-13 22:40:09 -0600

    Update DinersTablesSeeder.php

--- database/seeds/DinersTablesSeeder.php
+++ database/seeds/DinersTablesSeeder.php
@@ -14,114 +14,82 @@ class DinersTablesSeeder extends Seeder
     {
         Config::Create([
             'name' => 'Mesa 1',
-            'detail' => 'Mesa 1',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 1'
         ]);
 
         Config::Create([
             'name' => 'Mesa 2',
-            'detail' => 'Mesa 2',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 2'
         ]);
 
         Config::Create([
             'name' => 'Mesa 3',
-            'detail' => 'Mesa 3',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 3'
         ]);
 
         Config::Create([
             'name' => 'Mesa 4',
-            'detail' => 'Mesa 4',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 4'
         ]);
 
         Config::Create([
             'name' => 'Mesa 5',
-            'detail' => 'Mesa 5',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 5'
         ]);
 
         Config::Create([
             'name' => 'Mesa 6',
-            'detail' => 'Mesa 6',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 6'
         ]);
 
         Config::Create([
             'name' => 'Mesa 7',
-            'detail' => 'Mesa 7',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 7'
         ]);
 
         Config::Create([
             'name' => 'Mesa 8',
-            'detail' => 'Mesa 8',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 8'
         ]);
 
         Config::Create([
             'name' => 'Mesa 9',
-            'detail' => 'Mesa 9',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 9'
         ]);
 
         Config::Create([
             'name' => 'Mesa 10',
-            'detail' => 'Mesa 10',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 10'
         ]);
 
         Config::Create([
             'name' => 'Mesa 11',
-            'detail' => 'Mesa 11',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 11'
         ]);
 
         Config::Create([
             'name' => 'Mesa 12',
-            'detail' => 'Mesa 12',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 12'
         ]);
 
         Config::Create([
             'name' => 'Mesa 13',
-            'detail' => 'Mesa 13',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 13'
         ]);
 
         Config::Create([
             'name' => 'Mesa 14',
-            'detail' => 'Mesa 14',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 14'
         ]);
 
         Config::Create([
             'name' => 'Mesa 15',
-            'detail' => 'Mesa 15',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Mesa 15'
         ]);
 
         Config::Create([
             'name' => 'Barra',
-            'detail' => 'Barra',
-            'created_at' => '2024-01-01 01:01:01.000',
-            'updated_at' => '2024-01-01 01:01:01.000'
+            'detail' => 'Barra'
         ]);
     }
 }
