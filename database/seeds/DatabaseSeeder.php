f6442b6b1962102f5616590a6ab2014888080428
fitodark <fitodark68@gmail.com>
2024-01-13 22:38:10 -0600

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(ConfigSeeder::class);
         $this->call(BranchOfficersSeeder::class);
+        $this->call(DinersTablesSeeder::class);
     }
 }
